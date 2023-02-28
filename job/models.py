4e346660a533b64a63002b882a678398ae7297cf
Amiramohamed <96356137+amiramohamedar@users.noreply.github.com>
2023-02-28 12:42:40 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
+
+'''
+djang model field : # title
+-html widget # charfield input
+-valadtion  # email password or any input requierinput
+db size     #max_length=100
+'''
+JOB_TYPE= (
+    ('Full Time', 'Full Time'),
+    ('Part Time','Part Time'),
+)
+
 # Create your models here.
+class Job(models.Model): #تكافئ table
+    title = models.CharField(max_length=100 ) # coulmn يكافئ 
+    #location 
+    jop_type= models.CharField(max_length=15, choices=JOB_TYPE )
+    description = models.TextField (max_length=2000 ) 
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary=models .IntegerField(default=0)
+    experience = models.IntegerField (default=1)
+
+#عشان اظهر ال عنوان الوظيفه اللى هيا jop object اللى رجعلتى لما عملت save لل الوظيفه 
+    def __str__ (self):
+        return self.title
+
